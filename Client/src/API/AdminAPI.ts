ffae88255ce7e007821fa042f88ee1a6f7bb4e74
Priyang <patelpriyang95@gmail.com>
2022-04-09 23:28:31 +0530

    Admin Api

--- Client/src/API/AdminAPI.ts
+++ Client/src/API/AdminAPI.ts
@@ -8,3 +8,21 @@ export const LoadUsers = async () => {
     return error.response;
   }
 };
+
+export const AddProductCall = async (product: any) => {
+  try {
+    const response = await axios.post("/api/products/admin/add", product);
+    return response.data;
+  } catch (error: any) {
+    return error.response;
+  }
+};
+
+export const DeleteProductCall = async (id: any) => {
+  try {
+    const response = await axios.delete(`/api/products/product/${id}`);
+    return response.data;
+  } catch (error: any) {
+    return error.response;
+  }
+};
