c7aeaccca4665bbd6e2651a0d5fc2ab3b493cb30
Priyang <patelpriyang95@gmail.com>
2022-04-09 21:50:34 +0530

    useOnScreen

--- Client/src/Utils/CustomHooks.ts
+++ Client/src/Utils/CustomHooks.ts
@@ -126,3 +126,25 @@ export const useToggle = (
   };
   return [Toggle, toggleState, ChangeState];
 };
+
+// useOnScreen Hook
+// Usage: Check if element is on screen
+export const useOnScreen = (
+  ref: React.RefObject<HTMLElement>,
+  rootMargin = "0px"
+) => {
+  const [isIntersecting, setIntersecting] = useState(false);
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      ([entry]) => {
+        setIntersecting(entry.isIntersecting);
+      },
+      { rootMargin }
+    );
+    if (ref.current) observer.observe(ref.current);
+    return () => {
+      observer.disconnect();
+    };
+  }, [ref, rootMargin]);
+  return { isIntersecting };
+};
