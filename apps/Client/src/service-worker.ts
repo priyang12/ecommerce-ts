a9b8189340f6d68f4179dc6569728899e61278a7
Priyang <patelpriyang95@gmail.com>
2025-06-05 16:23:24 +0530

    vite-plugin-pwa for register service worker

--- apps/Client/src/service-worker.ts
+++ apps/Client/src/service-worker.ts
@@ -28,7 +28,7 @@ registerRoute(({ request, url }) => {
   } // Return true to signal that we want to use the handler.
 
   return true;
-}, createHandlerBoundToURL(process.env.PUBLIC_URL + "/index.html"));
+}, createHandlerBoundToURL("/index.html"));
 
 registerRoute(
   // Add in any other file extensions or routing criteria as needed.
--- apps/Client/vite.config.ts
+++ apps/Client/vite.config.ts
@@ -1,8 +1,23 @@
+import fs from "fs";
 import { defineConfig } from "vite";
+import { VitePWA } from "vite-plugin-pwa";
 import react from "@vitejs/plugin-react";
 
+const manifest = JSON.parse(
+  fs.readFileSync("../Client/public/manifest.json", "utf-8")
+);
+
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      srcDir: "src", // assuming your service-worker.ts is in src
+      filename: "service-worker.ts",
+      strategies: "injectManifest",
+      injectRegister: false, // because you register manually
+      manifest,
+    }),
+  ],
   server: {
     proxy: {
       "/api": {
