273bda7f7d6fc2919571d5ad5348783002c53d06
Priyang <patelpriyang95@gmail.com>
2025-06-08 15:05:33 +0530

    remove package

--- packages/validation/index.ts
+++
@@ -1,9 +0,0 @@
-import { z } from "zod";
-export * from "./Validators/UserValidation";
-export * from "./Validators/CartValidation";
-export * from "./Validators/ProductValidation";
-export * from "./Validators/WishlistValidation";
-export * from "./Validators/OrderValidation";
-export * from "./Validators/ReviewValidation";
-
-export { z };
